8f3d50f8673ce20e662c09dc13afc30f11ff5511
Maksym Studenets <maksym.studenets@gmail.com>
2024-12-03 11:04:50 +0200

    Code cleanup

--- src/main/java/co/inventorsoft/hoteloccupancy/dto/OccupancyRequestDto.java
+++ src/main/java/co/inventorsoft/hoteloccupancy/dto/OccupancyRequestDto.java
@@ -1,7 +1,6 @@
 package co.inventorsoft.hoteloccupancy.dto;
 
 import co.inventorsoft.hoteloccupancy.validation.GreaterThanZero;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
